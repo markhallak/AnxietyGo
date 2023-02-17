e590685f49eda6987f86e19d4e212197936c353d
abhinav1912 <abhinavmathur1912@gmail.com>
2023-02-17 19:23:20 +0530

    refactor: absolute import path

--- lib/models/challenge.dart
+++ lib/models/challenge.dart
@@ -1,12 +1,14 @@
-import 'difficulty.dart';
+import 'package:anxiety_go/models/difficulty.dart';
 
 class Challenge {
   final String name;
   final String description;
   final Difficulty difficulty;
+
   /// todo: category as enum
   final String category;
   final String thumbnailUrl;
 
-  const Challenge(this.name, this.description, this.difficulty, this.category, this.thumbnailUrl);
+  const Challenge(this.name, this.description, this.difficulty, this.category,
+      this.thumbnailUrl);
 }
