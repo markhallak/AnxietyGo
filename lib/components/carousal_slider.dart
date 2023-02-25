8a89898d11118e21747a748ca13a1404ab0d8bc7
Satvir Dhaliwal <satvir1229@gmail.com>
2023-02-25 20:59:51 +0530

    -Add null check in carousal file

--- lib/components/carousal_slider.dart
+++ lib/components/carousal_slider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
@@ -13,14 +13,17 @@ class AppCarousel extends StatefulWidget {
 }
 
 class _AppCarousel extends State<AppCarousel> {
+   List<String>? imageList = [];
   CarouselController carouselController = CarouselController();
 
-  final imageList = [
-    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
-    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
-    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
-  ];
-
+  @override
+  void initState() {
+    super.initState();
+    imageList = widget.imageList;
+    if(imageList==null || imageList!.isEmpty) {
+      imageList = addStaticImages();
+    }
+  }
   @override
   Widget build(BuildContext context) {
 
@@ -34,7 +37,7 @@ class _AppCarousel extends State<AppCarousel> {
 
             enableInfiniteScroll: false),
         items: imageList
-            .map((e) => Container(
+            ?.map((e) => Container(
                   color: Colors.white12,
                   child: Center(child: Image.network(e)),
                 ))
@@ -43,3 +46,13 @@ class _AppCarousel extends State<AppCarousel> {
     );
   }
 }
+
+// get static images if passes imageList os empty or null
+List<String> addStaticImages() {
+  return [
+    'https://images.unsplash.com/photo-1520342868574-5fa3804e551c?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=6ff92caffcdd63681a35134a6770ed3b&auto=format&fit=crop&w=1951&q=80',
+    'https://images.unsplash.com/photo-1522205408450-add114ad53fe?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=368f45b0888aeb0b7b08e3a1084d3ede&auto=format&fit=crop&w=1950&q=80',
+    'https://images.unsplash.com/photo-1519125323398-675f0ddb6308?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=94a1e718d89ca60a6337a6008341ca50&auto=format&fit=crop&w=1950&q=80',
+  ];
+}
+
