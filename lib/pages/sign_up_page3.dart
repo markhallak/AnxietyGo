43b61f01ad12e25d5f5f57e06ac71de28b61d148
chandansgowda <chandansuresh007@gmail.com>
2023-03-05 20:52:53 +0530

    initial setup done

---
+++ lib/pages/sign_up_page3.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class SignUpPage4 extends StatelessWidget {
+  const SignUpPage4({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Column(
+          children: const [
+            Text('AnxietyGo')
+          ]
+        ),
+    );
+  }
+}
