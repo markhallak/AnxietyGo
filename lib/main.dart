75acbad7640932baf84c19d103a2101b919f1886
chandansgowda <chandansuresh007@gmail.com>
2023-03-05 21:33:55 +0530

    basic UI setup done

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       title: 'AnxietyGo',
       home: SignUpPage4(),
     );
